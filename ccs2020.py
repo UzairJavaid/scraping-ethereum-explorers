0cceb02ba5d3d537753e9a798f894d76cab00c3a
Uzair Javaid <37859763+UzairJavaid@users.noreply.github.com>
2020-01-18 18:07:23 +0800

    Add files via upload

---
+++ ccs2020.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import csv
+
+
+class CcsSpider(scrapy.Spider):
+    name = 'ccs'
+    # apiUrl = 'https://blockchain.com/eth/address/{}'
+    apiUrl = 'https://blockchair.com/ethereum/address/{}'
+    start = []
+    
+    with open('keys.csv','rt') as f:
+        data = csv.reader(f)
+        for url in data:
+            addr = apiUrl.format(url[0])
+            start.append(addr)
+    
+    start_urls = start
+
+    def parse(self, response):
+        a = response.css('li.block__item')
+        b = a.css('span.not span::text').get()
+        c = a.css('span.not span.grey::text').get()
+        d = b + c
+        yield {
+            'feeEther': d
+        }
+
+    # def parse(self, response):
+    #     a = response.css('ul.block__ul')
+    #     yield {
+    #         'inTX': a.css('span a::text')[1].get()
+    #     }
+
+    # def parse(self, response):
+    #     a = response.css('div.grey')
+    #     b = a.css('span::text')[5].get()
+    #     c = a.css('span::text')[6].get()
+    #     d = b + c
+    #     yield {
+    #         'outEther': d
+    #     }
+
+    # def parse(self, response):
+    #     a = response.css('div.grey')
+    #     b = a.css('span::text')[0].get()
+    #     c = a.css('span::text')[1].get()
+    #     d = b+c
+    #     yield {
+    #         'inEther': d
+    #     }
+
+    # def parse(self, response):
+    #     a = response.css('ul.block__ul')
+    #     yield {
+    #         'countTX': a.css('li.block__item span::text')[13].get()
+    #     }
+    
+    # def parse(self, response):
+    #     a = response.css('div.sc-8sty72-0')
+    #     yield {
+    #         'countTX': a.css('span.sc-1ryi78w-0::text')[5].get()
+    #     }
